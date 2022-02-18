8b9c36180690dc82eab1b6e06688c77f3599f3b6
Rohit Kumar <rohit@rtbrick.com>
2022-02-18 16:31:40 +0530

    ID-8387 Default rbac file name change for lum

--- jwt_util.h
+++ jwt_util.h
@@ -5,7 +5,7 @@
 #include <string.h>
 
 #define JWT_DEFINITION_FILE      "/usr/share/rtbrick/tacplus/jwt_claims.json"
-#define JWT_RBAC_RTB_CMDS_FILE   "/usr/share/rtbrick/tacplus/rbac_role_rtb_cmds.json"
+#define JWT_RBAC_RTB_CMDS_FILE   "/usr/share/rtbrick/tacplus/rbac_role_cmds.json"
 #define JWT_SECRET_FILE          "/var/rtbrick/auth/secrets_jwks.json" 
 #define JWT_RTB_CMDS_LEN         2048
 #define LOGIN_USERNAME_LEN       100
